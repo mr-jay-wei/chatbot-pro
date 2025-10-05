fea359493de4c63463c91d2ad383878b6a8a233b
Jay <854326781@qq.com>
2025-10-06 03:28:06 +0800

    Nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ChatBot Next.js",
+  title: "ChatBot Nextjs",
   description: "A smart chatbot project",
 };
 
